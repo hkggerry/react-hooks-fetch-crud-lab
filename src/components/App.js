1b1745daa55e4f0e1cceafe17ac8bd9092ccf9d8
gerry <hkggerry@gmail.com>
2022-02-28 11:23:11 -0800

    Work in Progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [question, setQuestions] = useState([])
+  const api = "http://localhost:4000/questions"
+
+
+  useEffect(()=>{
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+  }, [])
+  
+  function handleDeleteItem(deletedQuestion){
+    const updatedQuestion = question.filter((item) => item.id !== deletedQuestion.id);
+      setQuestions(updatedQuestion);
+  }
+  
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList question={question} handleDeleteItem={handleDeleteItem}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({question}) {
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{question.map((question)=>(
+        <QuestionItem key={question.id} question={question}/>
+      ))}</ul>
     </section>
   );
 }
